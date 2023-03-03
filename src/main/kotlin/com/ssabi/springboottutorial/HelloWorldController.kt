55d583252e639bbb6bc4c6c2f2ae3a5d36e0380d
ssabii <fhy1930@gmail.com>
2023-03-03 23:02:25 +0900

    feat: HelloWorldController 구현

--- src/main/kotlin/com/ssabi/springboottutorial/HelloWorldController.kt
+++ src/main/kotlin/com/ssabi/springboottutorial/HelloWorldController.kt
@@ -1,5 +1,13 @@
 package com.ssabi.springboottutorial
 
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/hello")
 class HelloWorldController {
 
+  @GetMapping
+  fun helloWorld(): String = "Hello, this is a REST endpoint"
 }
