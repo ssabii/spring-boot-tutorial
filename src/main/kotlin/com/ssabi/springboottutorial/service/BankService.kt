5a2f899d94ecef287a96fa85dda176ef52ce59f4
ssabii <fhy1930@gmail.com>
2023-03-06 00:48:21 +0900

    chore: 불필요 import 제거

--- src/main/kotlin/com/ssabi/springboottutorial/service/BankService.kt
+++ src/main/kotlin/com/ssabi/springboottutorial/service/BankService.kt
@@ -3,7 +3,6 @@ package com.ssabi.springboottutorial.service
 import com.ssabi.springboottutorial.datasource.BankDataSource
 import com.ssabi.springboottutorial.model.Bank
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Service
 
 @Service
