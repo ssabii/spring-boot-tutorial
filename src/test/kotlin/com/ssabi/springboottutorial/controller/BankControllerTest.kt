3a4d6b0f020b20d310c8c3951960f54a54d474ca
ssabii <fhy1930@gmail.com>
2023-03-26 23:33:26 +0900

    chore: 테스트 코드 정리

--- src/test/kotlin/com/ssabi/springboottutorial/controller/BankControllerTest.kt
+++ src/test/kotlin/com/ssabi/springboottutorial/controller/BankControllerTest.kt
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
+import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.web.servlet.*
 
 @SpringBootTest
@@ -19,7 +20,6 @@ internal class BankControllerTest @Autowired constructor(
   @Autowired val mockMvc: MockMvc,
   @Autowired val objectMapper: ObjectMapper
 ) {
-
 
   val baseUrl = "/api/banks"
 
@@ -156,6 +156,7 @@ internal class BankControllerTest @Autowired constructor(
   inner class DeleteExistingBank {
 
     @Test
+    @DirtiesContext
     fun `should delete the bank with the given account number`() {
       val accountNumber = 1234
 
